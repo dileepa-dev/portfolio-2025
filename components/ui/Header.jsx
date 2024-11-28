197ae0d9d7715a28592c3d2e90a8ed305191503b
dileepaHsenid <dileepa.m@hsenidlanka.com>
2024-11-28 07:18:12 +0530

    Added basic files

---
+++ components/ui/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Link from 'next/link'
+import { Button } from './button'
+import Nav from './Nav'
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white">
+        <div className="container mx-auto flex justify-between items-center">
+            {/* Logo Icon */}
+            <Link href="/">
+            <h1 className='text-4xl font-semibold'>
+                Dileepa<span className="text-accent">.</span></h1>
+            </Link>
+            {/* Desktop Nav*/}
+            <div className="hidden xl:flex items-center gap-8">
+                <Nav/>
+                <Link href="/comtact">
+                    <Button>Hire Me</Button>
+                </Link>
+            </div>
+            {/* mobile Nav*/}
+            <div className="xl:hidden">Mobile Nav</div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: ["class"],
+    content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+  	extend: {
+		container: {
+			center: true,
+			padding: "15px",
+		},
+		screens: {
+			sm: "640px",
+			md: "768px",
+			lg: "960px",
+			xl: "1200px",
+		},
+		fontFamily: {
+			primary: "var(--font-jetbrainsMono)",
+		},
+		colors: {
+			primary: "#101422",
+		  accent: {
+			  DEFAULT: "#A8DCE7",
+			  hover: "#ooe187",
+		  },
+		  secondary: "#272B3B"
+		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
+  },
+  plugins: [require("tailwindcss-animate")],
+};
