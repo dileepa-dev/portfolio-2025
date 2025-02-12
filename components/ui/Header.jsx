a1b6388c785ad9547e4f6c875b2ceda4d448f604
Dileepa Malshan <dlpmalshan@gmail.com>
2025-02-12 16:03:03 +0530

    resume page added

--- components/ui/Header.jsx
+++ components/ui/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
             {/* Desktop Nav*/}
             <div className="hidden xl:flex items-center gap-8">
                 <Nav/>
-                <Link href="/comtact">
+                <Link href="/contact">
                     <Button>Hire Me</Button>
                 </Link>
             </div>
